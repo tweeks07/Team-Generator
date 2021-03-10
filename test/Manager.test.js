b8276a415645b4ce94f3022e1e86b241cde4ce0f
Taylor Weeks <taylorlweeks707@gmail.com>
2021-03-10 18:46:11 -0500

    passed manager test

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -2,15 +2,25 @@
 
 const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
+const { expect } = require("@jest/globals");
 
 test("Can set office number via constructor argument", () => {
+    const test = 1;
+    const manager = new Manager("Taylor", 7, "taylor@email.com", test);
+    expect(manager.officeNumber).toEqual(test);
 
 });
 
 test('getRole() should return "Manager"', () => {
+    const test = "Manager";
+    const manager = new Manager("Taylor", 7, "taylor@email.com", 777)
+    expect(manager.getRole()).toEqual(test);
 
 });
 
 test("Can get office number via getOffice()", () => {
+    const test = 1;
+    const manager = new Manager("Taylor", 7, "taylor@email.com", test)
+    expect(manager.getOfficeNumber()).toEqual(test);
 
 });
