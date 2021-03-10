930ecd1bd48e535e9088ddf1a465f0196b72ec30
Taylor Weeks <taylorlweeks707@gmail.com>
2021-03-10 17:57:24 -0500

    added json worked on code

--- app.js
+++ app.js
@@ -11,37 +11,137 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
+const employeeArray = [];
 
 // Write code to use inquirer to gather information about the development team members,
-inquirer.prompt([
-    {
-        type: 'input',
-        message: 'What is your name?',
-        name: 'employeeName',
-    },
-    {
-        type: 'input',
-        message: 'What is you ID number?',
-        name: 'employeeID',
-    },
-    {
-        type: 'input',
-        message: 'What is your email address?',
-        name: 'employeeEmail',
-    },
-    {
-        type: 'list',
-        message: 'What kind of employee are you?',
-        choices: ["Manager", "Engineer", "Intern"],
-    }
 
-])
-.then(answers =>
-    fs.writeFile('new.html', generateHTML
-    (answers), function(err){
-        if (err) return console.log(err);
-    }));
+function employeeRole () {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What kind of employee are you?',
+            choices: ["Manager", "Engineer", "Intern", "No More Employees"],
+        },
+    ]) .then(function (answers) {
+        if (answers.role === "Manager") {
+          managerQuestions()  
+        } else if (answers.role === "Engineer") {
+            engineerQuestions()
+        }else if (answers.role === "Intern") {
+            internQuestions()
+        }else if (answers.role === "No More Employees"){
+            writeHTML()
+        }
+    })
+}
+
+function managerQuestions () {
+   // .then(function (answers) {
+        //if (answers.role === "Manager") {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "managerName",
+                    message: "What is your name?",
+                },
+                {
+                    type: "input",
+                    name: "managerID",
+                    message: "What is your ID number?",
+                },
+                {
+                    type: "input",
+                    name: "managerEmail",
+                    message: "What is your email address?",
+                },
+                {
+                    type: "input",
+                    name: "officeNumber",
+                    message: "What is your office number?",
+                },
+            ]).then(function (answers){
+                const newManager = new Manager (answers.managerName, answers.managerID, answers.managerEmail, answers.officeNumber);
+                employeeArray.push(newManager)
+                employeeRole()
+  
+            })
+
+}
+
+function engineerQuestions () {
+    inquirer.prompt([
+        {
+            type:  "input",
+            name: "engineerName",
+            message: "What is your name?",
+        },
+        {
+            type: "input",
+            name: "engineerID",
+            message: "What is your ID number?"
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is your email address?",
+        },
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "What is your Github Username?",
+        },
+
+    ]).then(function (answers){
+        const newEngineer = new Engineer (answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub);
+        employeeArray.push(newEngineer)
+        employeeRole()
+    })
+
+        } 
+function internQuestions() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "What is your name?",
+        },
+        {
+            type: "input",
+            name: "internID",
+            message: "What is you ID number?",
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is your email address?",
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "What is the name of the school you attend?",
+        }
+    ]).then(function (answers){
+        const newIntern = new Intern (answers.internName, answers.internID, answers.internEmail, answers.internSchool);
+        employeeArray.push(newIntern)
+        employeeRole()
+    })
+}    
+        //else if (answers.role === "Intern") {
+    
+      
+
+function writeHTML (){
+    console.log(employeeArray)
+    const renderdEmployees = render(employeeArray)
+    console.log(renderdEmployees)
+    fs.writeFile(outputPath, renderdEmployees, err => {
+        if (err)console.log ("Error!")
+        else console.log ("File Written!")
+    } )
+}  
+                
+employeeRole();
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 // After the user has input all employees desired, call the `render` function (required
