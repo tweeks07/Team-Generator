7b35c606950c5b19ca86e57d65ec66305682deba
Taylor Weeks <taylorlweeks707@gmail.com>
2021-03-10 19:10:41 -0500

    passed engineer test

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,15 +1,25 @@
 // Starter file provided by Instructor (03/09/2021) AC
 
+const { expect } = require("@jest/globals");
 const Engineer = require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
+    const test = 1;
+    const engineer = new Engineer("Taylor", 7, "taylor@email.com", test);
+    expect(engineer.engineerGithub).toEqual(test);
 
 });
 
 test("getRole() should return \"Engineer\"", () => {
+    const test = "Engineer";
+    const engineer = new Engineer("Taylor", 7, "taylor@email.com", "tweeks07");
+    expect(engineer.getRole()).toEqual(test);
 
 });
 
 test("Can get GitHub username via getGithub()", () => {
+    const test = 1;
+    const engineer = new Engineer("Taylor", 7, "taylor@email.com", test);
+    expect(engineer.getGithub()).toEqual(test);
 
 });
